7d7402c0b7c34b4dcf1607aeb535f6c559582f43
Akash276 <48401041+Akash276@users.noreply.github.com>
2020-01-27 23:53:21 +0530

    Create a.cpp

---
+++ CPP/a.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<pii,int> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+
+#define MOD 1000000007
+#define EPF 1e-7
+#define pb push_back
+#define bitc _builtin_popcountll
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+#define fr(i,a,b) for(int i=(a),_b=b;i<=_b;i++)
+#define rep(i,n) for(int i=0,_n=(n);i<_n;i++)
+void solve(){
+
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0),cout.tie(0);
+#ifdef akash
+	cin.exceptions(cin.failbit);
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+	int t=1;
+	//cin>>t;
+	fr(i,1,t){
+		solve();
+	}
+}
